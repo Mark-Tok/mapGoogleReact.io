c36d88619fa3520cb809feb1da446d1c1d39f5ed
Mark <36090033+Mark-Tok@users.noreply.github.com>
2020-02-19 19:58:30 +0300

    Add files via upload

---
+++ precache-manifest.c6dd616d7741d7733df0665caa83411e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f8170f7b38d394d8a7eb44a027fe71",
+    "url": "/mapGoogleReact.io/index.html"
+  },
+  {
+    "revision": "951a14f0cff7ffc0626d",
+    "url": "/mapGoogleReact.io/static/css/main.3f93fea1.chunk.css"
+  },
+  {
+    "revision": "d9d56542c25bf10eaa32",
+    "url": "/mapGoogleReact.io/static/js/2.8d80dfc3.chunk.js"
+  },
+  {
+    "revision": "951a14f0cff7ffc0626d",
+    "url": "/mapGoogleReact.io/static/js/main.ae430249.chunk.js"
+  },
+  {
+    "revision": "ebb4b30d33d5523412f9",
+    "url": "/mapGoogleReact.io/static/js/runtime~main.5930353d.js"
+  }
+]);
